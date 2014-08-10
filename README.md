7720c32278be7224a868e73e6215ebe1ae0874ef
Rex Lee(李俊) <duguying2008@gmail.com>
2014-08-10 21:57:28 +0800

    the project starting...

---
+++ README.md
@@ -0,0 +1,7 @@
+# Websocket #
+
+This is a websocket server/client functions library. And there will be a server and client demo for using this library.
+
+# License #
+MIT License
+
---
+++ src/websocket.c
@@ -0,0 +1,14 @@
+/**
+ * this is demo server for websocket
+ * File Name: websocket.c
+ * Author: rex
+ * Mail: duguying2008@gmail.com 
+ * Created Time: 2014年08月10日 星期日 21时46分38秒
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+  printf("hello world.\n");
+  return 0;
+}
